55886c02fe52043fd730aa2f6988dc73dc268b71
happyloa <piafyoyo06@gmail.com>
2026-05-11 15:19:06 +0800

    feat: 專案作品 tabs

--- app/pages/project.vue
+++ app/pages/project.vue
@@ -1,4 +1,50 @@
 <template>
   <LayoutBanner />
+  <nav class="px-3 py-6 md:py-10">
+    <ul
+      class="mx-auto flex max-w-[1296px] overflow-x-auto whitespace-nowrap text-center text-heading-xxx-small [scrollbar-width:none] md:text-heading-xx-small"
+    >
+      <li class="flex-1">
+        <button
+          type="button"
+          class="w-full px-3 py-2 transition hover:bg-primary hover:text-neutral-0 md:py-3"
+        >
+          全部
+        </button>
+      </li>
+      <li class="flex-1">
+        <button
+          type="button"
+          class="w-full px-3 py-2 transition hover:bg-primary hover:text-neutral-0 md:py-3"
+        >
+          平面設計
+        </button>
+      </li>
+      <li class="flex-1">
+        <button
+          type="button"
+          class="w-full px-3 py-2 transition hover:bg-primary hover:text-neutral-0 md:py-3"
+        >
+          網頁設計
+        </button>
+      </li>
+      <li class="flex-1">
+        <button
+          type="button"
+          class="w-full px-3 py-2 transition hover:bg-primary hover:text-neutral-0 md:py-3"
+        >
+          前端切版
+        </button>
+      </li>
+      <li class="flex-1">
+        <button
+          type="button"
+          class="w-full px-3 py-2 transition hover:bg-primary hover:text-neutral-0 md:py-3"
+        >
+          後端開發
+        </button>
+      </li>
+    </ul>
+  </nav>
   <h1 class="text-primary underline">專案作品</h1>
 </template>
