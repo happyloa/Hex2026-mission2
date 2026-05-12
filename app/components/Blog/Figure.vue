ec40f412aa8d3c712e3ba01dcfb2b4ac3a940051
happyloa <piafyoyo06@gmail.com>
2026-05-12 14:39:55 +0800

    feat: 新增部落格文章內容資料

---
+++ app/components/Blog/Figure.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  },
+  caption: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <figure class="mb-5 lg:mb-6">
+    <img :src="src" :alt="alt" class="mb-2 block lg:mb-4" />
+    <figcaption>{{ caption }}</figcaption>
+  </figure>
+</template>
